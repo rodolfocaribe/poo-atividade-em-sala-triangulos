eeba8c732633fe4883cb40d3780c5d11b5e34638
Rodolfo C Ribeiro <rodolfo.ribeiro@estudante.iftm.edu.br>
2022-10-19 12:01:01 -0300

    movendo metodos para classe Triangulo

--- src/main/java/br/triangulos/App.java
+++ src/main/java/br/triangulos/App.java
@@ -1,2 +1,18 @@
-package br.triangulos;public class App {
+package br.triangulos;
+
+import javax.swing.JOptionPane;
+
+
+public class App {
+    public static void main(String[] args) {
+
+    }
+
+    static void leLados() {
+        int lado1 = Integer.parseInt(JOptionPane.showInputDialog("Digite o lado 1: "));
+        int lado2 = Integer.parseInt(JOptionPane.showInputDialog("Digite o lado 2: "));
+        int lado3 = Integer.parseInt(JOptionPane.showInputDialog("Digite o lado 3: "));
+        Triangulo triangulo = new Triangulo(lado1, lado2, lado3);
+
+    }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,9 @@
 module br.triangulos {
     requires javafx.controls;
     requires javafx.fxml;
-            
-                            
+    requires java.desktop;
+
+
     opens br.triangulos to javafx.fxml;
     exports br.triangulos;
 }
